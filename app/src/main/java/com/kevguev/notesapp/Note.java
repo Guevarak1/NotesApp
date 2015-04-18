5103dc14673de482444b9e55803c5a7fe9698690
Kevin Guevara <guevarak12@gmail.com>
2015-04-18 17:09:01 -0400

    layouts added

---
+++ app/src/main/java/com/kevguev/notesapp/Note.java
@@ -0,0 +1,7 @@
+package com.kevguev.notesapp;
+
+/**
+ * Created by Kevin Guevara on 4/18/2015.
+ */
+public class Note {
+}
---
+++ app/src/main/java/com/kevguev/notesapp/NotesDBAdapter.java
@@ -0,0 +1,137 @@
+package com.kevguev.notesapp;
+
+/**
+ * Created by Kevin Guevara on 4/18/2015.
+ */
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class NotesDBAdapter {
+
+    public static final String KEY_ROWID = "_id";
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_CONTENT = "content";
+    public static final String KEY_TIME = "time";
+
+    private static final String TAG = "NotesDbAdapter";
+    private DatabaseHelper mDbHelper;
+    private SQLiteDatabase mDb;
+
+    private static final String DATABASE_NAME = "Notes Database";
+    private static final String SQLITE_TABLE = "Notes";
+    private static final int DATABASE_VERSION = 1;
+
+    private final Context mCtx;
+
+    private static final String DATABASE_CREATE =
+            "CREATE TABLE if not exists " + SQLITE_TABLE + " (" +
+                    KEY_ROWID + " integer PRIMARY KEY autoincrement," +
+                    KEY_TITLE + "," +
+                    KEY_CONTENT + "," +
+                    KEY_TIME + "," +
+                    " UNIQUE (" + KEY_TITLE +"));";
+
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+
+        DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            Log.w(TAG, DATABASE_CREATE);
+            db.execSQL(DATABASE_CREATE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
+                    + newVersion + ", which will destroy all old data");
+            db.execSQL("DROP TABLE IF EXISTS " + SQLITE_TABLE);
+            onCreate(db);
+        }
+    }
+
+    public NotesDBAdapter(Context ctx) {
+        this.mCtx = ctx;
+    }
+
+    public NotesDBAdapter open() throws SQLException {
+        mDbHelper = new DatabaseHelper(mCtx);
+        mDb = mDbHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close() {
+        if (mDbHelper != null) {
+            mDbHelper.close();
+        }
+    }
+
+    public long createNote(String code, String name,
+                              String continent) {
+
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(KEY_TITLE, code);
+        initialValues.put(KEY_CONTENT, name);
+        initialValues.put(KEY_TIME, continent);
+
+        return mDb.insert(SQLITE_TABLE, null, initialValues);
+    }
+
+    public boolean deleteAllNotes() {
+        int doneDelete = 0;
+        doneDelete = mDb.delete(SQLITE_TABLE, null , null);
+        Log.w(TAG, Integer.toString(doneDelete));
+        return doneDelete > 0;
+
+    }
+
+    public Cursor fetchNotesByName(String inputText) throws SQLException {
+        Log.w(TAG, inputText);
+        Cursor mCursor = null;
+        if (inputText == null  ||  inputText.length () == 0)  {
+            mCursor = mDb.query(SQLITE_TABLE, new String[] {KEY_ROWID,
+                            KEY_TITLE, KEY_CONTENT, KEY_TIME},
+                    null, null, null, null, null);
+
+        }
+        else {
+            mCursor = mDb.query(true, SQLITE_TABLE, new String[] {KEY_ROWID,
+                            KEY_TITLE, KEY_CONTENT, KEY_TIME},
+                    KEY_CONTENT + " like '%" + inputText + "%'", null,
+                    null, null, null, null);
+        }
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+
+    }
+
+    public Cursor fetchAllNotes() {
+
+        Cursor mCursor = mDb.query(SQLITE_TABLE, new String[] {KEY_ROWID,
+                        KEY_TITLE, KEY_CONTENT, KEY_TIME},
+                null, null, null, null, null);
+
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+    }
+
+    public void insertSomeCountries() {
+
+        createNote("Hard coded note","hard coded note blabla","need to program this");
+
+    }
+
+}
